b6c052e0d6ffd3ad4fad28786fc9ebd3faf6af5e
dominguesguilherme <guilhermes.domingues@gmail.com>
2024-06-30 18:41:03 -0300

    chore: add doctrine and mysql dependencies

--- app/docker/php/entrypoint.sh
+++ app/docker/php/entrypoint.sh
@@ -11,3 +11,18 @@ composer install \
 echo "- Done"
 
 exec "$@"
+
+# If running via docker-compose, then it is necessary to wait for the mysql container to be available
+echo "- Waiting for mysql in host '$DB_SERVER'"
+until mysql -u $DB_USER -p$DB_PASSWORD -h $DB_SERVER --port=$DB_PORT -e "exit" >&2
+do
+    echo "-- MySQL is unavailable - sleeping"
+    sleep 1
+done
+echo "- Done! MySQL is up - executing command"
+
+# echo "- Running Migrations"
+# /app/bin/console doctrine:migrations:migrate --no-interaction
+# echo "- Done"
+
+exec "$@"
